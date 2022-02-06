9ce39bef6c12d410396b135d74c52027e5f20408
sreenitha-reddy-gavi-reddy <96791385+sreenitha-reddy-gavi-reddy@users.noreply.github.com>
2022-02-06 18:17:15 +0530

    #include<stdio.h> #include<math.h> int main() {     int a,b,c;     scanf("%d",&a);     b=sqrt(a);     c=b*b;     if(c==a)     {         printf("True");     }     else     {         printf("False");     }     return 0; }

---
+++ Square_Number.c
@@ -0,0 +1,18 @@
+#include<stdio.h>
+#include<math.h>
+int main()
+{
+    int a,b,c;
+    scanf("%d",&a);
+    b=sqrt(a);
+    c=b*b;
+    if(c==a)
+    {
+        printf("True");
+    }
+    else
+    {
+        printf("False");
+    }
+    return 0;
+}
