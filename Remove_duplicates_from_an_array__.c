5ab2a9964f950ea00bb68b31beac1ed3cf68fd73
sreenitha-reddy-gavi-reddy <96791385+sreenitha-reddy-gavi-reddy@users.noreply.github.com>
2022-02-06 18:41:49 +0530

    #include<stdio.h> int main() {     int arr[20],i,j,k,size;     scanf("%d",&size);     for(i=0;i<size;i++)     {         scanf("%d",&arr[i]);     }     for(i=0;i<size;i++)     {         for(j=i+1;j<size;j++)         {             if(arr[i]==arr[j])             {                 for(k=j;k<size-1;k++)                 {                     arr[k]=arr[k+1];                 }                 size--;                 j--;             }         }     }     for(i=0;i<size;i++)     {         printf("%d ",arr[i]);     }     return 0; }

---
+++ Remove_duplicates_from_an_array__.c
@@ -0,0 +1,30 @@
+#include<stdio.h>
+int main()
+{
+    int arr[20],i,j,k,size;
+    scanf("%d",&size);
+    for(i=0;i<size;i++)
+    {
+        scanf("%d",&arr[i]);
+    }
+    for(i=0;i<size;i++)
+    {
+        for(j=i+1;j<size;j++)
+        {
+            if(arr[i]==arr[j])
+            {
+                for(k=j;k<size-1;k++)
+                {
+                    arr[k]=arr[k+1];
+                }
+                size--;
+                j--;
+            }
+        }
+    }
+    for(i=0;i<size;i++)
+    {
+        printf("%d ",arr[i]);
+    }
+    return 0;
+}
