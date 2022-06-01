ead2894835c0cb058d73eb82a4d1833b87f5a1fd
sreenitha-reddy-gavi-reddy <96791385+sreenitha-reddy-gavi-reddy@users.noreply.github.com>
2022-06-01 22:25:40 +0530

    #include<stdio.h> #include<math.h> int rev(int); int power(int); int sqroot(int); int main() {     int n,r1=0,r2=0,sq=0,p=0;     scanf("%d",&n);     p=power(n);     r1=rev(p);     sq=(int)sqroot(r1);     r2=rev(sq);     if(n==r2)     printf("True");     else     printf("False"); } int power(int n) {     int p;     p=n*n; } int sqroot(int n) {     int sq=0;     sq=(int)sqrt(n);     return sq; } int rev(int n) {     int temp=0,r=0,t=0;     temp=n;     while(temp>0)     {         r=temp%10;         t=t*10+r;         temp=temp/10;     }     return t; }

--- Adam_number.c
+++ Adam_number.c
@@ -1,28 +1,41 @@
 #include<stdio.h>
+#include<math.h>
+int rev(int);
+int power(int);
+int sqroot(int);
 int main()
 {
-    int num,r,k=0,s=1,u,v=0,t=1;
-    scanf("%d",&num);
-    s=num*num;
-    while(num!=0)
-    {
-        r=num%10;
-        k=k*10+r;
-        num=num/10;
-    }
-    t=k*k;
-    while(t!=0)
-    {
-        u=t%10;
-        v=v*10+u;
-        t=t/10;
-    }
-    if(s==v)
-    {
-        printf("True");
-    }
+    int n,r1=0,r2=0,sq=0,p=0;
+    scanf("%d",&n);
+    p=power(n);
+    r1=rev(p);
+    sq=(int)sqroot(r1);
+    r2=rev(sq);
+    if(n==r2)
+    printf("True");
     else
+    printf("False");
+}
+int power(int n)
+{
+    int p;
+    p=n*n;
+}
+int sqroot(int n)
+{
+    int sq=0;
+    sq=(int)sqrt(n);
+    return sq;
+}
+int rev(int n)
+{
+    int temp=0,r=0,t=0;
+    temp=n;
+    while(temp>0)
     {
-        printf("False");
+        r=temp%10;
+        t=t*10+r;
+        temp=temp/10;
     }
+    return t;
 }
