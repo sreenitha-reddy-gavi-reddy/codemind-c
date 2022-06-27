7062a5c4cb6afec17d99367c636619e779520719
sreenitha-reddy-gavi-reddy <96791385+sreenitha-reddy-gavi-reddy@users.noreply.github.com>
2022-06-27 21:42:14 +0530

    #include<stdio.h> int main() {     int n,i,arr[100];     float avg,sum=0;     scanf("%d",&n);     for(i=0;i<n;i++)     {         scanf("%d",&arr[i]);     }     for(i=0;i<n;i++)     {         sum=sum+arr[i];     }     avg=sum/n;     printf("%.2f",avg); }

---
+++ average_of_the_array.c
@@ -0,0 +1,17 @@
+#include<stdio.h>
+int main()
+{
+    int n,i,arr[100];
+    float avg,sum=0;
+    scanf("%d",&n);
+    for(i=0;i<n;i++)
+    {
+        scanf("%d",&arr[i]);
+    }
+    for(i=0;i<n;i++)
+    {
+        sum=sum+arr[i];
+    }
+    avg=sum/n;
+    printf("%.2f",avg);
+}
