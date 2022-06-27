f3049d0bdd8fa9815557aa3f99de5ce0ab8f305e
sreenitha-reddy-gavi-reddy <96791385+sreenitha-reddy-gavi-reddy@users.noreply.github.com>
2022-06-27 22:31:44 +0530

    #include<stdio.h> int even_count(int *arr,int n) {     int i,c=0,k;     for(i=1;i<n-1;i++)     {         if(arr[i-1]%2==0 && arr[i+1]%2==0)         {             if(arr[i]%2==0)             {                 k++;             }         }     }     return k; } int main() {     int n,i,arr[100],k;     scanf("%d",&n);     for(i=0;i<n;i++)     {         scanf("%d",&arr[i]);     }     k=even_count(arr,n);     printf("%d",k); }

---
+++ count_even_numbers_between_even_numbers.c
@@ -0,0 +1,27 @@
+#include<stdio.h>
+int even_count(int *arr,int n)
+{
+    int i,c=0,k;
+    for(i=1;i<n-1;i++)
+    {
+        if(arr[i-1]%2==0 && arr[i+1]%2==0)
+        {
+            if(arr[i]%2==0)
+            {
+                k++;
+            }
+        }
+    }
+    return k;
+}
+int main()
+{
+    int n,i,arr[100],k;
+    scanf("%d",&n);
+    for(i=0;i<n;i++)
+    {
+        scanf("%d",&arr[i]);
+    }
+    k=even_count(arr,n);
+    printf("%d",k);
+}
