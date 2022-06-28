0fa4ef95d1bb13dbee03c3904db12d6e8d62e5cc
sreenitha-reddy-gavi-reddy <96791385+sreenitha-reddy-gavi-reddy@users.noreply.github.com>
2022-06-28 21:40:04 +0530

    #include<stdio.h> int digit_count(int n) {     int d,c=0;     while(n!=0)     {         d=n%10;         n=n/10;         c++;     }     return c; } int reverse(int n) {     int d,m=0;     while(n!=0)     {         d=n%10;         n=n/10;         m=m*10+d;     }     return m; } int main() {     int n,m,sq,p,c=0,d,k,r;     scanf("%d",&n);     sq=n*n;     m=n;     c=digit_count(n);     while(sq!=0)     {         d=sq%10;         sq=sq/10;         r=r*10+d;         p++;         if(p==c)         {             break;         }     }     k=reverse(r);     if(k==m)     {         printf("Automorphic Number");     }     else     {         printf("Not an Automorphic Number");     } }

--- Automorphic_Number.c
+++ Automorphic_Number.c
@@ -1,23 +1,51 @@
 #include<stdio.h>
-#include<stdlib.h>
-#include<math.h>
+int digit_count(int n)
+{
+    int d,c=0;
+    while(n!=0)
+    {
+        d=n%10;
+        n=n/10;
+        c++;
+    }
+    return c;
+}
+int reverse(int n)
+{
+    int d,m=0;
+    while(n!=0)
+    {
+        d=n%10;
+        n=n/10;
+        m=m*10+d;
+    }
+    return m;
+}
 int main()
 {
-    int n;
+    int n,m,sq,p,c=0,d,k,r;
     scanf("%d",&n);
-    int sqr,temp,last;
-    int count=0;
-    sqr=n*n;
-    temp=n;
-    while(temp>0)
+    sq=n*n;
+    m=n;
+    c=digit_count(n);
+    while(sq!=0)
     {
-        count++;
-        temp=temp/10;
+        d=sq%10;
+        sq=sq/10;
+        r=r*10+d;
+        p++;
+        if(p==c)
+        {
+            break;
+        }
     }
-    int flag=floor(pow(10,count));
-    last=sqr%flag;
-    if(last==n)
-    printf("Automorphic Number");
+    k=reverse(r);
+    if(k==m)
+    {
+        printf("Automorphic Number");
+    }
     else
-    printf("Not an Automorphic Number");
+    {
+        printf("Not an Automorphic Number");
+    }
 }
