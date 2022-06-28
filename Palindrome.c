5202769e48b3d98a16276028eb49ed1454b253c0
sreenitha-reddy-gavi-reddy <96791385+sreenitha-reddy-gavi-reddy@users.noreply.github.com>
2022-06-28 21:11:42 +0530

    #include<stdio.h> int main() {     int n,r,s=0,temp=0;     scanf("%d",&n);     temp=n;     while(n>0)     {         r=n%10;         s=s*10+r;         n=n/10;     }     if(temp==s)      printf("True");     else      printf("False"); }

--- Palindrome.c
+++ Palindrome.c
@@ -1,22 +1,17 @@
 #include<stdio.h>
 int main()
 {
-    int n,rev=0,rem,temp;
+    int n,r,s=0,temp=0;
     scanf("%d",&n);
     temp=n;
-    while(n!=0)
+    while(n>0)
     {
-        rem=n%10;
-        rev=rev*10+rem;
-        n/=10;
+        r=n%10;
+        s=s*10+r;
+        n=n/10;
     }
-    if(rev==temp)
-    {
-        printf("True");
-    }
+    if(temp==s)
+     printf("True");
     else
-    {
-        printf("False");
-    }
-    return 0;
+     printf("False");
 }
