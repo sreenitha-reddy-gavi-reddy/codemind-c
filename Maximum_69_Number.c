70453c4343ddff2bb1350389cdba0625fa0beff9
sreenitha-reddy-gavi-reddy <96791385+sreenitha-reddy-gavi-reddy@users.noreply.github.com>
2022-06-28 21:21:23 +0530

    #include<stdio.h> #include<math.h> int main() {     int n,d,r,k=0;     scanf("%d",&n);     d=(int)log10(n);     while(n>0)     {         r=n/(int)ceil(pow(10,d));         if(r==6&&k==0)         {             printf("9");             k=1;         }         else         {             printf("%d",r);         }         n=n%(int)ceil(pow(10,d));         d--;     } }

--- Maximum_69_Number.c
+++ Maximum_69_Number.c
@@ -2,19 +2,21 @@
 #include<math.h>
 int main()
 {
-    int n,d,r,flag=0;
+    int n,d,r,k=0;
     scanf("%d",&n);
     d=(int)log10(n);
     while(n>0)
     {
         r=n/(int)ceil(pow(10,d));
-        if(r==6 && flag==0)
+        if(r==6&&k==0)
         {
             printf("9");
-            flag=1;
+            k=1;
         }
         else
-        printf("%d",r);
+        {
+            printf("%d",r);
+        }
         n=n%(int)ceil(pow(10,d));
         d--;
     }
