eb145d8ce4a6f40eb2c89709d6e3ff0aad59f081
sreenitha-reddy-gavi-reddy <96791385+sreenitha-reddy-gavi-reddy@users.noreply.github.com>
2022-06-28 21:01:10 +0530

    #include<stdio.h> int sum(int *arr,int n) {     int i,sum=0;     for(i=0;i<n;i++)     {             sum=sum+arr[i];     }     return sum; } int main() {     int n,i,arr[100],k;     scanf("%d",&n);     for(i=0;i<n;i++)     {         scanf("%d",&arr[i]);     }     k=sum(arr,n);     printf("%d",k); }

---
+++ Sum_of_Array.c
@@ -0,0 +1,21 @@
+#include<stdio.h>
+int sum(int *arr,int n)
+{
+    int i,sum=0;
+    for(i=0;i<n;i++)
+    {
+            sum=sum+arr[i];
+    }
+    return sum;
+}
+int main()
+{
+    int n,i,arr[100],k;
+    scanf("%d",&n);
+    for(i=0;i<n;i++)
+    {
+        scanf("%d",&arr[i]);
+    }
+    k=sum(arr,n);
+    printf("%d",k);
+}
