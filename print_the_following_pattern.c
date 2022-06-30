e447e2ed46fc45e591d263d1158e85bb129ad2b1
sreenitha-reddy-gavi-reddy <96791385+sreenitha-reddy-gavi-reddy@users.noreply.github.com>
2022-06-30 23:51:14 +0530

    #include<stdio.h> int main() {     int n,i,j;     scanf("%d",&n);     for(i=1;i<=n;i++)     {         for(j=1;j<=n;j++)         {             printf("%c ",(64+i));         }         printf(" ");     } }

--- print_the_following_pattern.c
+++ print_the_following_pattern.c
@@ -1,17 +1,13 @@
 #include<stdio.h>
 int main()
 {
-    int i,n,j;
+    int n,i,j;
     scanf("%d",&n);
     for(i=1;i<=n;i++)
     {
-        for(j=1;j<=n-2;j++)
+        for(j=1;j<=n;j++)
         {
-            printf("%d",j);
-        }
-        for(j=n-3;j>=1;j--)
-        {
-            printf("%d",j);
+            printf("%c ",(64+i));
         }
         printf("
 ");
