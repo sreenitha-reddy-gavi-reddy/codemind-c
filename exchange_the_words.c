97b812d83cfea188fa31353aee33085f21bbfae6
sreenitha-reddy-gavi-reddy <96791385+sreenitha-reddy-gavi-reddy@users.noreply.github.com>
2022-07-02 22:15:52 +0530

    #include<stdio.h> #include<string.h> int main() {     char s[100];     scanf("%[^ ]s",s);     int i,j,c=1,d=0,l,a[100];     l=strlen(s);     a[0]=l;     for(i=l-1;i>=0;i--)     {         if(s[i]==' ')         {             a[c]=i;             c++;         }     }     for(i=l-1;i>=0;i--)     {         if(s[i]==' ' || i==0)         {             if(i==0)             {                 i=-1;             }             for(j=i+1;j<a[d];j++)             {                 printf("%c",s[j]);             }             d++;             if(i!=0)             {                 printf(" ");             }         }     }
    }

---
+++ exchange_the_words.c
@@ -0,0 +1,39 @@
+#include<stdio.h>
+#include<string.h>
+int main()
+{
+    char s[100];
+    scanf("%[^
+]s",s);
+    int i,j,c=1,d=0,l,a[100];
+    l=strlen(s);
+    a[0]=l;
+    for(i=l-1;i>=0;i--)
+    {
+        if(s[i]==' ')
+        {
+            a[c]=i;
+            c++;
+        }
+    }
+    for(i=l-1;i>=0;i--)
+    {
+        if(s[i]==' ' || i==0)
+        {
+            if(i==0)
+            {
+                i=-1;
+            }
+            for(j=i+1;j<a[d];j++)
+            {
+                printf("%c",s[j]);
+            }
+            d++;
+            if(i!=0)
+            {
+                printf(" ");
+            }
+        }
+    }
+    
+}
