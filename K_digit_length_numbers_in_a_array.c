3ad50b0c03a70e6f5269ca78e117b8630b73d999
sreenitha-reddy-gavi-reddy <96791385+sreenitha-reddy-gavi-reddy@users.noreply.github.com>
2022-07-15 15:04:43 +0530

    #include<stdio.h> int main() {     int n,i,a[100],c,d,t,k,x=0;     scanf("%d%d",&n,&k);     for(i=0;i<n;i++)     {         scanf("%d",&a[i]);     }     for(i=0;i<n;i++)     {         c=0;         t=a[i];         if(a[i]==0)         {             c=1;         }         while(t!=0)         {             c++;             d=t%10;             t=t/10;         }         if(c==k)         {             x++;         }     }     printf("%d",x); }

---
+++ K_digit_length_numbers_in_a_array.c
@@ -0,0 +1,30 @@
+#include<stdio.h>
+int main()
+{
+    int n,i,a[100],c,d,t,k,x=0;
+    scanf("%d%d",&n,&k);
+    for(i=0;i<n;i++)
+    {
+        scanf("%d",&a[i]);
+    }
+    for(i=0;i<n;i++)
+    {
+        c=0;
+        t=a[i];
+        if(a[i]==0)
+        {
+            c=1;
+        }
+        while(t!=0)
+        {
+            c++;
+            d=t%10;
+            t=t/10;
+        }
+        if(c==k)
+        {
+            x++;
+        }
+    }
+    printf("%d",x);
+}
