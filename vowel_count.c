201fd105e8c3480a204b998a11718dc8a544a6db
sreenitha-reddy-gavi-reddy <96791385+sreenitha-reddy-gavi-reddy@users.noreply.github.com>
2022-07-16 13:53:26 +0530

    #include<stdio.h> int main() {     char str[100];     int i,vowels=0;     scanf("%[^ ]s",&str);     for(i=0;str[i];i++)     {         if(str[i]=='a' || str[i]=='e'||str[i]=='i'|| str[i]=='o'||str[i]=='u'||str[i]=='A'||str[i]=='E'||str[i]=='I'||str[i]=='O'|| str[i]=='U')         {             vowels++;         }     }     printf("%d ",vowels);     return 0; }

---
+++ vowel_count.c
@@ -0,0 +1,18 @@
+#include<stdio.h>
+int main()
+{
+    char str[100];
+    int i,vowels=0;
+    scanf("%[^
+]s",&str);
+    for(i=0;str[i];i++)
+    {
+        if(str[i]=='a' || str[i]=='e'||str[i]=='i'|| str[i]=='o'||str[i]=='u'||str[i]=='A'||str[i]=='E'||str[i]=='I'||str[i]=='O'|| str[i]=='U')
+        {
+            vowels++;
+        }
+    }
+    printf("%d
+",vowels);
+    return 0;
+}
